a264edd3f99b7549f619f336f297e821a8a48170
Andrea Rampin <andrea.rampin@gmail.com>
2024-09-05 23:30:20 +1000

    formatting

--- client/src/components/bot_status.tsx
+++ client/src/components/bot_status.tsx
@@ -5,6 +5,7 @@ export const BotStatus = () => {
   const { isFetching, isError, data } = useQuery({ queryKey: ['bot-status'], queryFn: fetchStatus })
 
   if (isFetching) return <span className="text-xs text-yellow-600 block font-bold">Bot unknown</span>
+  
   if (isError) return <span className="text-xs text-red-600 block font-bold">Bot offline</span>
 
   if (data.ok) return <span className="text-xs text-green-600 block font-bold">Bot online</span>
